bf2c5beaca04b6d67c18a5343773ecc8e3af853a
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2023-09-22 13:48:13 +0200

    Started on testfunctions, non working. Need to add exception handling when entering a negative integer

--- src/test/java/com/OskarJohansson/DiceGameMkII/Control/inputIntTest.java
+++ src/test/java/com/OskarJohansson/DiceGameMkII/Control/inputIntTest.java
@@ -5,9 +5,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class inputIntTest {
 
@@ -23,8 +27,8 @@ public class inputIntTest {
         outContent = new ByteArrayOutputStream();
         originalOut = System.out;
         System.setOut(new PrintStream(outContent));
-
-        scanner = new Scanner(System.in);
+        String fakeInput = "";
+        scanner = new Scanner(fakeInput);
     }
     @After
     public void tearDown() {
@@ -36,9 +40,35 @@ public class inputIntTest {
     }
 
     @Test
-    public void testInputIntValidInput(){
+    public void testInputIntValidInput() {
 
         String fakeInput = "42\n";
+        System.setIn(new ByteArrayInputStream(fakeInput.getBytes()));
+
+        String userInput = scanner.nextLine();
+
+        assertEquals(42, userInput);
+
+        assertEquals("", outContent.toString());
+
+    }
+
+    @Test
+    public void testInputIntNonIntegerInput() {
+
+        String fakeInput = "NotANumber\n"; // This is not a valid integer input
+        System.setIn(new ByteArrayInputStream(fakeInput.getBytes()));
+
+        // Use the fake scanner as input source
+        scanner.next(fakeInput);
+
+        // Call the inputInt method
+        int result = userInput.inputInt();
+
+        // Assert that the result is the default value (0 in this case)
+        assertEquals(0, result);
+        // Assert that the error message is printed
+        assertTrue(outContent.toString().contains("That's not a number!"));
     }
 
 }
